5e32686a403e1e6823aec2857169bdc648f32fa6
soler marjorie <soler.marjorie81@gmail.com>
2023-09-07 17:58:55 +0200

    change mvc

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 
-require_once __DIR__."/templates/header.php";
-require_once __DIR__."/templates/footer.php";
+define('_ROOTPATH_', __DIR__);
+
+spl_autoload_register();
+
+use App\Controller\Controller;
+
+$controller = new Controller();
+$controller->route();
+
+?>
