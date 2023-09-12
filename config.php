c7ee766be14cace487e237f82c8e44e31e799813
soler marjorie <soler.marjorie81@gmail.com>
2023-09-12 18:12:40 +0200

    new site garage parrot

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    return [
-        'db_name' => 'garageParrot',
-        'db_user' => 'root',
-        'db_password' => '',
-        'db_port' => '3306',
-        'db_host' => 'localhost'
-    ];
--- templates/pages/témoignages.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once __DIR__."/templates/header.php";
-require_once __DIR__."/templates/footer.php";
